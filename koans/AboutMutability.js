0cbfa223c3a9cc2c939fc11fff77e1f7ddaf3cc9
Emily Yang <emilyayang@gmail.com>
2019-06-06 23:44:47 -0700

    "finished koans"

--- koans/AboutMutability.js
+++ koans/AboutMutability.js
@@ -4,7 +4,7 @@ describe("About Mutability", function() {
     var aPerson = {firstname: "John", lastname: "Smith" };
     aPerson.firstname = "Alan";
     
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should understand that constructed properties are public and mutable", function () {
@@ -16,7 +16,7 @@ describe("About Mutability", function() {
     var aPerson = new Person ("John", "Smith");
     aPerson.firstname = "Alan";
     
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should expect prototype properties to be public and mutable", function () {
@@ -30,13 +30,13 @@ describe("About Mutability", function() {
     };
     
     var aPerson = new Person ("John", "Smith");
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("John Smith");
     
     aPerson.getFullName = function() {
       return this.lastname + ", " + this.firstname;
     };
     
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("Smith, John");
   });
 
   it("should know that variables inside a constructor and constructor args are private", function () {
@@ -48,21 +48,37 @@ describe("About Mutability", function() {
       this.getLastName  = function() { return lastname; };
       this.getFullName  = function() { return fullName; };
     }
-    var aPerson = new Person ("John", "Smith");
+    var aPerson = new Person ("John", "Smith");//the key: creates a new person using Person
+// only thing you created was an object with only methods on it, not even attributes, like firstname, lastname. So your object aPerson is like this:
 
-    aPerson.firstname = "Penny";
+// {
+//     getFirstName: function(), // => John
+//     getLastName: function(), // => Smith
+//     getFullName: function() // => John Smith
+// }
+
+    aPerson.firstname = "Penny";//only updates new Person aPerson
     aPerson.lastname  = "Andrews";
     aPerson.fullName  = "Penny Andrews";
-    
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
 
-    aPerson.getFullName = function() {
+//     {
+//     firstname: "Penny", //////new attributes.
+//     lastname: "Andrews",
+//     fullName: "Penny Andrews",
+//     getFirstName: function(), // => John
+//     getLastName: function(), // => Smith
+//     getFullName: function() // => John Smith
+// }
+    
+    expect(aPerson.getFirstName()).toBe("John");//return what was originally set as firstname for Person, so aPerson
+    expect(aPerson.getLastName()).toBe("Smith");
+    expect(aPerson.getFullName()).toBe("John Smith");
+
+    aPerson.getFullName = function() { //takes from the new instance of Person(Penny)
       return aPerson.lastname + ", " + aPerson.firstname;
     };
     
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("Andrews, Penny");
   });
 
 });
